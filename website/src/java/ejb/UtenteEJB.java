ba71b39f17a448fd96445870f5e046b6bca794a9
gerardomanzo <gerardo.manzo@outlook.com>
2017-12-21 14:17:36 +0100

    Aggiunti controlli registrazione utente e aggiunto metodo per login
    Spostato campo $class in Utente.java.
    La registrazione fallisce se vengono forniti nomeUtente o email già registrati.

--- website/src/java/ejb/UtenteEJB.java
+++ website/src/java/ejb/UtenteEJB.java
@@ -1,11 +1,11 @@
 package ejb;
 
 import bean.Utente;
+import com.google.gson.Gson;
 import java.net.URI;
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -14,24 +14,72 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
 @Stateless
+@LocalBean
 public class UtenteEJB {
 
-	private final static String UTENTE_CLASS = "it.unisa.Utente";
-	private final static URI URI = UriBuilder.fromUri("http://169.51.23.2:31090/api/Utente").port(31090).build();
+	private final static String URI_QUERY = "http://169.51.23.2:31090/api/queries";
+	private final static URI URI_UTENTE = UriBuilder.fromUri("http://169.51.23.2:31090/api/Utente").port(31090).build();
 	private final static Client CLIENT = ClientBuilder.newClient();
 
 	@Inject
 	private IdGenerator idGen;
 
-	public void creaUtente(Utente utente) {
-		JsonObject obj = Json.createObjectBuilder()
-				.add("$class", UTENTE_CLASS)
-				.add("idUtente", idGen.nextId())
-				.add("nomeUtente", utente.getNomeUtente())
-				.add("email", utente.getEmail())
-				.add("password", utente.getPassword())
-				.build();
+	private final Gson gson = new Gson();
 
-		Response response = CLIENT.target(URI).request().post(Entity.entity(obj, MediaType.APPLICATION_JSON));
+	public Utente creaUtente(Utente utente) {
+
+		String body;
+		Utente[] result;
+
+		Response responseEmail = CLIENT.target(URI_QUERY + "/findByEmail")
+				.queryParam("param", utente.getEmail())
+				.request()
+				.get();
+
+		body = responseEmail.readEntity(String.class);
+
+		result = gson.fromJson(body, Utente[].class);
+
+		if (result.length == 0) {
+			Response responseNomeUtente = CLIENT.target(URI_QUERY + "/findByUsername")
+					.queryParam("param", utente.getNomeUtente())
+					.request()
+					.get();
+
+			body = responseNomeUtente.readEntity(String.class);
+
+			result = gson.fromJson(body, Utente[].class);
+
+			if (result.length == 0) {
+				utente.setIdUtente(idGen.nextId());
+
+				String json = gson.toJson(utente);
+
+				Response response = CLIENT.target(URI_UTENTE)
+						.request()
+						.post(Entity.entity(json, MediaType.APPLICATION_JSON));
+				return utente;
+			}
+			return null;
+		}
+		return null;
+	}
+
+	public Utente cercaUtente(Utente utente) {
+
+		Response response = CLIENT.target(URI_QUERY + "/findByEmailPassword")
+				.queryParam("email", utente.getEmail())
+				.queryParam("password", utente.getPassword())
+				.request()
+				.get();
+
+		String body = response.readEntity(String.class);
+
+		Utente[] result = gson.fromJson(body, Utente[].class);
+
+		if (result.length > 0) {
+			return result[0];
+		}
+		return null;
 	}
 }
--- website/src/java/servlet/RegistrazioneServlet.java
+++ website/src/java/servlet/RegistrazioneServlet.java
@@ -5,23 +5,20 @@ import ejb.PasswordHash;
 import ejb.UtenteEJB;
 import java.io.IOException;
 import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class RegistrazioneServlet extends HttpServlet {
 
 	@Inject
 	private UtenteEJB utenteEJB;
-	
+
 	@Inject
 	private PasswordHash pwHash;
-
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,7 +26,21 @@ public class RegistrazioneServlet extends HttpServlet {
 		String email = request.getParameter("email");
 		String password = pwHash.hash(request.getParameter("password"));
 
-		utenteEJB.creaUtente(new Utente(nomeUtente, email, password));
+		Utente utente = new Utente(nomeUtente, email, password);
+
+		utente = utenteEJB.creaUtente(utente);
+
+		if (utente != null) {
+			HttpSession session = request.getSession();
+			session.removeAttribute("utente");
+			session.setAttribute("utente", utente);
+
+			RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/index.html");
+			dispatcher.forward(request, response);
+		} else {
+			RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/registrazione.html");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
