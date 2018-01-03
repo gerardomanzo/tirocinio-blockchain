eea74eb73437c393aa526a610f19f14eefe7a279
gerardomanzo <gerardo.manzo@outlook.com>
2018-01-03 11:47:55 +0100

    Implementata transazione confermaRegistrazioneUtente

---
+++ business-network-definition/lib/logic.js
@@ -0,0 +1,17 @@
+/**
+ * Conferma la registrazione di un utente.
+ * @param {it.unisa.ConfermaRegistrazioneUtente} tx
+ * @transaction
+ */
+function confermaRegistrazioneUtente(tx) {
+	var utente = tx.utente;
+	
+	utente.statoRegistrazione = true;
+
+	// Get the participant registry for the participant.
+	return getParticipantRegistry('it.unisa.Utente')
+		.then(function (participantRegistry) {
+			// Update the asset in the participant registry.
+			return participantRegistry.update(utente);
+		});
+}
