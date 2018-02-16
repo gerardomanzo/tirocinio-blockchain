a1999b2b96982c0668fcaa1c6d2b85f974610824
gerardomanzo <gerardo.manzo@outlook.com>
2018-02-16 17:02:14 +0100

    Aggiornato OggettoEJB

--- website/src/java/ejb/OggettoEJB.java
+++ website/src/java/ejb/OggettoEJB.java
@@ -1,8 +1,12 @@
 package ejb;
 
 import bean.Oggetto;
+import bean.Partecipazione;
 import com.google.gson.Gson;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -44,11 +48,87 @@ public class OggettoEJB {
 
             response = CLIENT.target(URI_BASE + "/Oggetto")
                     .request()
-                    .post(Entity.entity(json, MediaType.APPLICATION_JSON));
+                    .post(Entity.entity(result, MediaType.WILDCARD_TYPE).entity(json, MediaType.APPLICATION_JSON));
             return oggetto;
         }
         return null;
+    }
 
+    public List<Oggetto> cercaOggettiNonRegistrati() {
+        Response response = CLIENT.target(URI_QUERY + "/findObjectNotRegistred")
+                .request()
+                .get();
+
+        String body = response.readEntity(String.class);
+
+        Oggetto[] result = gson.fromJson(body, Oggetto[].class);
+
+        return Arrays.asList(result);
+
+    }
+
+    public void confermaOggetto(String idOggetto) {
+        String json = "{\"oggetto\": \"resource:it.unisa.Oggetto#" + idOggetto + "\"}";
+
+        Response response = CLIENT.target(URI_BASE + "/ConfermaRegistrazioneOggetto")
+                .request()
+                .post(Entity.entity(json, MediaType.APPLICATION_JSON));
+
+    }
+
+    public List<Oggetto> cercaTuttiGliOggetti(String idEvento, String idUtente) {
+
+        Response response = CLIENT.target(URI_BASE + "/Oggetto")
+                .request()
+                .get();
+
+        String body = response.readEntity(String.class);
+
+        Oggetto[] oggetti = gson.fromJson(body, Oggetto[].class);
+
+        List<Oggetto> oggettiUtente = new ArrayList<>();
+
+        for (Oggetto o : oggetti) {
+            if (o.getProprietario().contains(idUtente)) {
+                oggettiUtente.add(o);
+            }
+        }
+
+        response = CLIENT.target(URI_BASE + "/Partecipazione")
+                .request()
+                .get();
+
+        body = response.readEntity(String.class);
+
+        Partecipazione[] partecipazioni = gson.fromJson(body, Partecipazione[].class);
+
+        List<Partecipazione> partecipazioniEvento = new ArrayList<>();
+
+        for (Partecipazione o : partecipazioni) {
+            if (o.getIdEvento().contains(idEvento)) {
+                partecipazioniEvento.add(o);
+            }
+        }
+
+        List<Oggetto> lista = new ArrayList<>();
+
+        Boolean trovato;
+
+        for (Oggetto o : oggettiUtente) {
+            trovato = false;
+
+            for (Partecipazione p : partecipazioniEvento) {
+                if (p.getIdOggetto().contains(o.getIdOggetto())) {
+                    trovato = true;
+                }
+            }
+
+            if (!trovato) {
+                lista.add(o);
+            }
+        }
+
+        return lista;
     }
 
 }
