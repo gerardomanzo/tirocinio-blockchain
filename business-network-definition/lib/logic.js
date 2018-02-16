e4d08c91b89cc318d02bb8e3068555f7226705e0
gerardomanzo <gerardo.manzo@outlook.com>
2018-02-16 16:12:28 +0100

    Implementate transazioni

--- business-network-definition/lib/logic.js
+++ business-network-definition/lib/logic.js
@@ -1,4 +1,26 @@
 /**
+ * Aggiunge un criterio di valutazione ad un evento.
+ * @param {it.unisa.AggiungiCriterio} tx
+ * @transaction
+ */
+function aggiungiCriterio(tx) {
+	var evento = tx.evento;
+	var criterio = tx.criterio;
+
+	if(evento.criteri)
+    	evento.criteri.push(criterio);
+	else
+    	evento.criteri = [criterio];
+
+	// Get the asset registry for the asset.
+	return getAssetRegistry('it.unisa.Evento')
+		.then(function (assetRegistry) {
+			// Update the asset in the asset registry.
+			return assetRegistry.update(evento);
+		});
+}
+
+/**
  * Conferma la registrazione di un utente.
  * @param {it.unisa.ConfermaRegistrazioneUtente} tx
  * @transaction
@@ -13,5 +35,41 @@ function confermaRegistrazioneUtente(tx) {
 		.then(function (participantRegistry) {
 			// Update the asset in the participant registry.
 			return participantRegistry.update(utente);
+		});
+}
+
+/**
+ * Conferma la registrazione di un oggetto.
+ * @param {it.unisa.ConfermaRegistrazioneOggetto} tx
+ * @transaction
+ */
+function confermaRegistrazioneOggetto(tx) {
+	var oggetto = tx.oggetto;
+
+	oggetto.statoRegistrazione = true;
+
+	// Get the asset registry for the asset.
+	return getAssetRegistry('it.unisa.Oggetto')
+		.then(function (assetRegistry) {
+			// Update the asset in the asset registry.
+			return assetRegistry.update(oggetto);
+		});
+}
+
+/**
+ * Conferma la registrazione di una partecipazione.
+ * @param {it.unisa.ConfermaRegistrazionePartecipazione} tx
+ * @transaction
+ */
+function ConfermaRegistrazionePartecipazione(tx) {
+	var partecipazione = tx.partecipazione;
+
+	partecipazione.statoRegistrazione = true;
+
+	// Get the asset registry for the asset.
+	return getAssetRegistry('it.unisa.Partecipazione')
+		.then(function (assetRegistry) {
+			// Update the asset in the asset registry.
+			return assetRegistry.update(partecipazione);
 		});
 }
